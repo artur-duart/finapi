9e9846498f8281410f03932b0ba31bafa3561422
Artur Duarte <arturduartemoraes@gmail.com>
2023-06-29 15:42:28 -0300

    ✨ feat: add post account route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 'use strict';
 
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+app.use(express.json());
 const port = 3333;
+
+const customers = [];
+
+// cpf - String
+// name - String
+// id - uuid
+// statement []
+
+app.post('/account', (req, res) => {
+	const { cpf, name } = req.body;
+	const id = uuidv4();
+	customers.push({
+		cpf,
+		name,
+		id,
+		statement: [],
+	});
+
+	return res.status(201).send();
+});
 
 app.listen(port);
